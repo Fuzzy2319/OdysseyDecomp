31a409758626014b66d684409bbea5a47c4bd844
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2025-07-08 16:48:59 +0200

    add:KuriboHack partial implementation

--- src/Player/CollisionShapeKeeper.h
+++ src/Player/CollisionShapeKeeper.h
@@ -52,6 +52,8 @@ public:
     const CollidedShapeResult* getCollidedShapeResult(s32 index) const;
     const CollidedShapeResult* getCollidedShapeSupportResult(s32 index) const;
 
+    s32 getNumCollideResult() const { return mNumCollideResult; }
+
 private:
     sead::Vector3f mBoundingCenter = {0.0f, 0.0f, 0.0f};
     f32 mBoundingRadius = 0.0f;
